cee51e6a6ff7b11303cd8c29dd68088f620ed01e
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-07-01 22:38:07 -0700

    baby steps in c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Return 0 if successful, otherwise return EOF
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
