f464036718901a90b6fccba24f7716defc3ddc11
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-07-01 22:18:34 -0700

    corrected errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+/**
+ * function_name - main
+ * parameter void - takes no argument
+ *
+ * main is the entry point
+ * main returns 0 upon succesful execution and returns EOF on error
+ */
 int main(void)
-
 {
-	char str[] = """\"Programming is like building a multilingual puzzle";
-
-	puts(str);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
