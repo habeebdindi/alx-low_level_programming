d663b65c61db5f8ce8d5745273347c453cb8dd24
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-07-01 22:52:19 -0700

    subtle change

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 /**
  * main - entry point
  *
- * Return: Return 0 if successful, otherwise return EOF
+ * Return: Return 0 if successful, otherwise return error
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
+	fprintf(stderr, "error");
 	return (0);
 }
