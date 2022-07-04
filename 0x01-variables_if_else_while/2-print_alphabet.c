411015e16430feebf4b2a688ba7d735144320e73
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-07-04 04:31:43 -0700

    program prnts using putchar only

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: 0 if successful and EOF on error
+ */
+int main(void)
+{
+	int letters;
+
+	letters = 'a';
+	while (letters <= 'z')
+	{
+		putchar(letters);
+		letters++;
+	}
+	return (0);
+}
