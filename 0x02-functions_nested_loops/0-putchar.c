c84022ab39b1c6b236ab8b091d4d8ad0a516457a
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-07-05 04:47:39 -0700

    A printer and a readme have beenadded

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: returns 0 always
+ */
+
+int main(void)
+{
+	char var1 = '_';
+	char var2 = 'p';
+	char var3 = 'u';
+	char var4 = 't';
+	char var5 = 'c';
+	char var6 = 'h';
+	char var7 = 'a';
+	char var8 = 'r';
+
+	/**
+	 * putchar - prints single character values
+	 *
+	 * Return: returns results of argument
+	 */
+	putchar(var1);
+	putchar(var2);
+	putchar(var3);
+	putchar(var4);
+	putchar(var5);
+	putchar(var6);
+	putchar(var7);
+	putchar(var8);
+	putchar('\n');
+	return (0);
+}
