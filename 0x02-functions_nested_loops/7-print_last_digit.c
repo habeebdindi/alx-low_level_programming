3fbb2b5048aa707e7c05d29fab6c0ae0f4056aa1
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-07-05 21:00:38 -0700

    update

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - prints the last digit of an input
- * @i: input 
+ * @i: input
  *
  * Return: returns i
  */
@@ -11,13 +11,13 @@ int print_last_digit(int i)
 {
 	if (i >= 0)
 	{
-	_putchar (i % 10);
+	_putchar (i % 10 + '0');
 	return (i % 10);
 	}
 	else
 	{
 	i = i * -1;
-	_putchar (i % 10);
+	_putchar (i % 10 + '0');
 	return (i % 10);
 	}
 }
