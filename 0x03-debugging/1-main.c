7d16caf6b6c21dad3af7848dcc6b26ea869d8b6b
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-07-07 18:13:11 -0700

    Bulk submission of tasks

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,17 +12,13 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 	i = 0;
 
-	while (i < 10)
-		/**
-		 * seeing an infinite loop?
-		 * It's because an increment statement 
-		 * was not set.
-		 * add an increment statement to solve 
-		 * this problem.
-		 */
-	{
-	putchar(i);
-	}
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,5 +1,5 @@
 ##THIS DIRECTORY CONTAINS ALL ALX-HOLBERTON TASKS ON DEBUGGING
 
 #Debugging is an important skill every programmer is meant to have
-#The files in this directory show the implementation of manual 
+#The files in this directory show the implementation of manual
 #debugging.
