dccfe4c27415e6d9176bd5cb48fe5842ba60063d
Habeeb Hassan Dindi <habeebullahdindi@gmail.com>
2022-07-22 18:25:07 -0700

    baby steps in recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: input string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	if (*s != '\0')
+	{
+		if (*s <= 'z')
+		{
+			_print_rev_recursion(s + 1);
+			_putchar (*s);
+		}
+	}
+}
