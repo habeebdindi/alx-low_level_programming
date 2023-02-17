0e7fafad6c0b308e35536662309045a6436cac14
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-02-17 02:16:13 +0000

    Add all basic and advanced scripts and programs

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	write(2, "\"and that piece of art is useful\" - Dora Korpar, \
+	2015-10-19\n", strlen("\"and that piece of art is useful\" - \
+	Dora Korpar, 2015-10-19\n"));
+
+	return (1);
+}
