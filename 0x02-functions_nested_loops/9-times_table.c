51b205316d4b9170c3183c22fdcbf729c4ecaae8
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-02-22 02:06:31 +0000

    Fix the code to produce desired output

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,30 +17,30 @@ void times_table(void)
 		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
-
 		num = i;
 		j = 0;
 		while (j < 9)
 		{
 			if (num <= 9)
+			{
 				_putchar(num + '0');
+				if (j != 8)
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (num + i < 10)
+						_putchar(' ');
+				}
+			}
 			else if (num > 9)
 			{
 				_putchar((num / 10) + '0');
 				_putchar((num % 10) + '0');
-			}
-			if (num == 0 && j != 8)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			if (num != i * 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (num <= 9)
+				if (j != 8)
+				{
+					_putchar(',');
 					_putchar(' ');
+				}
 			}
 			num += i;
 			j++;
