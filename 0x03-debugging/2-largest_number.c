b76b493382a2959952f7a740afac1e644223a5dc
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-02-23 17:07:25 +0000

    Change conditions of 'if' construct

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -13,17 +13,17 @@ int largest_number(int a, int b, int c)
 	int largest;
 
 	if (a == b && b == c)
-		largest = a;
+		largest = c;
 
-	if ((a > b && b > c) || (a > c && c > b) || (a > b && b == c))
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if ((b > a && a > c) || (b > c && c > a) || (b > a && a == c))
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	else if ((c > b && b > a) || (c > a && a > b) || (c > b && b == a))
+	else if (c > b && c > a)
 	{
 		largest = c;
 	}
