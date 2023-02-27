545bdb299e2e18188913ae11b0eea123cb954265
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-02-27 09:57:02 +0000

    Add last advanced program

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i, j;
+
+	i = 0;
+	while(i < 99)
+	{
+		j = 1;
+		while(j < 100)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+			}
+			if (j > i)
+			{
+				if (i != j)
+				{
+					putchar((j / 10) + '0');
+					putchar(j % 10 + '0');
+
+					if (!(i == 98 && j == 99))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
