4ee41c2f074d213b971df23f80e216c3f193da91
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-05 13:00:27 +0000

    Refactor code

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,29 +2,32 @@
 #include <limits.h>
 
 /**
+ * pr_uint - Prints unsinged integer using _putchar.
+ * @n: Integer to be printed.
+ *
+ * Return: void
+ */
+
+void pr_uint(unsigned int n)
+{
+	if (n / 10 != 0)
+		pr_uint(n / 10);
+	_putchar((n % 10) + 48);
+}
+
+
+/**
  * print_number - Prints integer using _putchar.
  * @n: Integer to be printed.
  *
  * Return: void
  */
 
-
-
-void pr_int(int n) {
-    if (n < 0) {
-        putchar('-');
-        n = -n;
-    }
-    if (n / 10 != 0)
-        pr_int(n / 10);
-    putchar((n % 10) + 48);
-}
-
 void print_number(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n = -n;
 	}
 	pr_uint((unsigned int) n);
