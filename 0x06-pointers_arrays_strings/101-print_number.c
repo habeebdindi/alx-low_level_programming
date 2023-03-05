953b687bacde082237131c598b63309715146574
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-05 12:59:11 +0000

    Add helper function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,23 +2,26 @@
 #include <limits.h>
 
 /**
- * print_number - Prints integer using _putchar.
+ * pr_uint - Prints unsinged integer using _putchar.
  * @n: Integer to be printed.
  *
  * Return: void
  */
 
-
-
-void pr_int(int n) {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    if (n / 10 != 0)
-        pr_int(n / 10);
-    _putchar((n % 10) + 48);
+void pr_uint(unsigned int n)
+{
+	if (n / 10 != 0)
+		pr_uint(n / 10);
+	_putchar((n % 10) + 48);
 }
+
+
+/**
+ * print_number - Prints integer using _putchar.
+ * @n: Integer to be printed.
+ *
+ * Return: void
+ */
 
 void print_number(int n)
 {
@@ -27,5 +30,5 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	pr_int((unsigned int) n);
+	pr_uint((unsigned int) n);
 }
