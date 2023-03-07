069aac0765ca2530fe9dc519cfe9b0c0edb06513
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-07 12:28:38 +0000

    Refactor code

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,31 +15,15 @@ char *_strstr(char *haystack, char *needle)
 {
 	char *ptr;
 	char *mtc;
-	int len = 0;
-	int rec = 0;
-	int check = 0;
 
-	mtc = needle;
-	while (*mtc)
+	while (*haystack)
 	{
-		mtc++;
-		len++;
-	}
-	ptr = haystack;
-	while (*ptr)
-	{
-		check = rec;
-		for (mtc = needle; *mtc != '\0'; mtc++)
-			if (*ptr == *mtc)
-				rec++;
-		if (check == rec)
-			rec = 0;
-		else if (rec == len)
-		{
-			ptr -= len - 1;
-			return (ptr);
-		}
-		ptr++;
+		ptr = haystack;
+		for (mtc = needle; *ptr && *mtc && (*ptr == *mtc); ptr++, mtc++)
+			;
+		if (*mtc == '\0')
+			return (haystack);
+		haystack++;
 	}
 	return (NULL);
 }
