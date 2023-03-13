7d6bc975b3499865b1892a24d6e553261daf8645
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-13 15:00:24 +0000

    Modify to print just 50 times in total

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,13 +14,13 @@ int main(void)
 	fib1 = 1;
 	fib2 = 2;
 	printf("%ld, %ld, ", fib1, fib2);
-	for (i = 0; i < 50; i++)
+	for (i = 0; i < 48; i++)
 	{
 		tmp = fib1;
 		fib1 = fib2;
 		fib2 += tmp;
 		printf("%ld", fib2);
-		if (i != 49)
+		if (i != 47)
 			printf(", ");
 	}
 	putchar('\n');
