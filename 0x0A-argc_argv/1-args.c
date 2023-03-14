248c61c711017a2013420467036794f8edd8adfd
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-14 12:50:16 +0000

    Update documentation

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,6 +4,8 @@
  * main - program prints no of arguments passed to it.
  * @argc: agrument count to main.
  * @argv: arguments to main
+ *
+ * Return: 0 always.
  */
 
 int main(int argc, __attribute__((unused)) char *argv[])
