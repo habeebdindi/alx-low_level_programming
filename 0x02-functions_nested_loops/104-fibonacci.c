1273c77364fe1f8c7b354cb245388af7a5656701
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-16 23:08:14 +0000

    Fix coding style errros and remove comments

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,38 +9,31 @@
  *
  * Return: Always 0.
  */
-int diglen(long unsigned int n);
 int main(void)
 {
 
-	long unsigned int fib1 = 1, fib1_l = 0, fib1_h = 0;
-	long unsigned int fib2 = 2, fib2_l = 0, fib2_h = 0;
-	long unsigned int tmp = 0, tmp_h = 0, tmp_l = 0, hi_n = 0, lo_n = 0;
+	unsigned int long fib1 = 1, fib1_l = 0, fib1_h = 0, fib2 = 2;
+	unsigned int long tmp = 0, tmp_h = 0, tmp_l = 0, hi_n = 0, lo_n = 0;
 	int i;
 
 	printf("%lu, %lu, ", fib1, fib2);
 	for (i = 3; i < 99; i++)
 	{
-       		if (i <= 92)
-       		{
-       			tmp = fib1;
-       			fib1 = fib2;
-       			fib2 += tmp;
-			/* if (i < 91) */
-		       	printf("%lu", fib2);
+		if (i <= 92)
+		{
+			tmp = fib1;
+			fib1 = fib2;
+			fib2 += tmp;
+			printf("%lu", fib2);
 			if (i == 91)
 			{
 				tmp_h = fib2 / div;
 				tmp_l = fib2 % div;
-				/* printf("\t91_h: %lu\t", tmp_h); */
-				/* printf("\t91_l: %lu\t", tmp_l); */
 			}
 			if (i == 92)
 			{
 				fib1_h = fib2 / div;
 				fib1_l = fib2 % div;
-				/* printf("\t92_l: %lu\t", fib1_h); */
-				/* printf("\t92_l: %lu\t", fib2_l); */
 			}
 		}
 		if (i >= 93)
