064639f3cc5e1820cc1cf25f7fa59356de17a4b0
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-21 08:52:01 +0000

    Handle case for when pointer to struct points to NULL

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+#include <stddef.h>
 /**
  * init_dog - initializes a variable of type 'struct dog'
  * @d: a pointer to the struct.
@@ -12,6 +12,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
