9fc8d88bf0e93da58c81f5c3902ab7e510a16342
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-21 11:36:39 +0000

    Store 2 parameters in the malloc'd struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,9 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stddef.h>
+
+char *_strdup(char *str);
+
 /**
  * new_dog - creates a new dog.
  * @name: a pointer to the dog's name.
@@ -13,12 +16,45 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *ptrname;
+	char *ptrowner;
 	dog_t *ptr = malloc(sizeof(dog_t));
 
+	if (name == NULL)
+		return (NULL);
+	if (owner == NULL)
+		return (NULL);
 	if (ptr == NULL)
 		return (NULL);
-	ptr->name = name;
+
+	ptrname = _strdup(name);
+	ptrowner = _strdup(owner);
+	if (ptrowner == NULL)
+		return (NULL);
+	if (ptrname == NULL)
+		return (NULL);
+
+	ptr->name = ptrname;
 	ptr->age = age;
-	ptr->owner = owner;
+	ptr->owner = ptrowner;
 	return (ptr);
+}
+
+char *_strdup(char *str)
+{
+	char *cpy, *tmp;
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[len] != '\0')
+		len++;
+	cpy = malloc(sizeof(char) * (len + 1));
+	if (cpy == NULL)
+		return (NULL);
+	tmp = cpy;
+	while (*str)
+		*tmp++ = *str++;
+	*tmp = '\0';
+	return (cpy);
 }
