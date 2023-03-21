51adb790f7d682f7e7365081036cad8af9511240
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-21 19:32:48 +0000

    Replace all string.h functions with custom defined ones

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,10 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stddef.h>
-#include <string.h>
+
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
 /**
  * new_dog - creates a new dog.
  * @name: a pointer to the dog's name.
@@ -23,23 +26,61 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (ptr == NULL)
 		return (NULL);
 
-	ptr->name = malloc(strlen(name) + 1);
+	ptr->name = malloc(_strlen(name) + 1);
 	if (ptr->name == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	strcpy(ptr->name, name);
+	_strcpy(ptr->name, name);
 
 	ptr->age = age;
 
-	ptr->owner = malloc(strlen(owner) + 1);
+	ptr->owner = malloc(_strlen(owner) + 1);
 	if (ptr->owner == NULL)
 	{
 		free(ptr->name);
 		free(ptr);
 		return (NULL);
 	}
-	strcpy(ptr->owner, owner);
+	_strcpy(ptr->owner, owner);
 	return (ptr);
+}
+
+
+/**
+ * _strcpy - Copies string pointed to by source to dest.
+ * @dest: Buffer to copy to.
+ * @src: Buffer to copy from.
+ *
+ * Return: void.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = src[i];
+
+	return (dest);
+}
+
+
+/**
+ * _strlen - Counts length of a string.
+ * @s: The string.
+ *
+ * Return: length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
 }
