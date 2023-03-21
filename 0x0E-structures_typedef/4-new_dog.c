491a5fd1f518fe8b6baf8e5a2f8ea5f761e9402f
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-21 19:17:28 +0000

    Try using strcpy in place of strdup and free any memory allocated if NULL

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,9 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stddef.h>
-
-char *_strdup(char *str);
-
+#include <string.h>
 /**
  * new_dog - creates a new dog.
  * @name: a pointer to the dog's name.
@@ -16,57 +14,32 @@ char *_strdup(char *str);
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *ptrname;
-	char *ptrowner;
 	dog_t *ptr;
 
-	if (name == NULL)
-		return (NULL);
-	if (owner == NULL)
-		return (NULL);
-
-	ptrname = _strdup(name);
-	ptrowner = _strdup(owner);
-	if (ptrowner == NULL)
-		return (NULL);
-	if (ptrname == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
 		return (NULL);
-	ptr->name = ptrname;
+
+	ptr->name = malloc(strlen(name) + 1);
+	if (ptr->name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	strcpy(ptr->name, name);
+
 	ptr->age = age;
-	ptr->owner = ptrowner;
+
+	ptr->owner = malloc(strlen(owner) + 1);
+	if (ptr->owner == NULL)
+	{
+		free(ptr->name);
+		free(ptr);
+		return(NULL);
+	}
+	strcpy(ptr->owner, owner);
 	return (ptr);
-}
-
-
-/**
- * _strdup -  returns a pointer to a newly allocated space in memory, which
- * contains a copy of the string given as a parameter.
- * @str: string given as parameter.
- *
- * Return: NULL if str = NULL. On success, the _strdup function returns a
- * pointer to the duplicated string. It returns NULL if insufficient memory was
- * available.
- */
-
-char *_strdup(char *str)
-{
-	char *cpy, *tmp;
-	int len = 0;
-
-	if (str == NULL)
-		return (NULL);
-	while (str[len] != '\0')
-		len++;
-	cpy = malloc(sizeof(char) * (len + 1));
-	if (cpy == NULL)
-		return (NULL);
-	tmp = cpy;
-	while (*str)
-		*tmp++ = *str++;
-	*tmp = '\0';
-	return (cpy);
 }
