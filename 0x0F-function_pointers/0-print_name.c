9ea695f02ab367f5f8b8d449d4af9eaf35fa863b
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-03-22 22:49:06 +0000

    Correct typo error in function description

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 
 /**
  * print_name - prints a name.
- * @name - pointer to the name.
+ * @name: pointer to the name.
  * @f: a pointer to a function that accepts a pointer to a char as an argument
  * and returns nothing.
  *
