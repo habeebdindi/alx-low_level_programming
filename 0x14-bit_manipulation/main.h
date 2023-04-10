9a1dae2d59aa658d22b650572e528ca9d4c67968
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-04-10 10:28:07 +0000

    Add header file

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+int _pow(int x, int y);
+int _strlen(char *s);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+
+#endif /* MAIN_H */
