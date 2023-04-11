6bec294c51e2f2fb0d7bce985b01bde484b862ae
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-04-11 14:41:37 +0000

    Remove else statement

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,8 +21,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 		nbytes = write(fd, text_content, strlen(text_content));
-	else
-		nbytes = write(fd, "\0", 1);
 
 	if (nbytes == -1)
 	{
