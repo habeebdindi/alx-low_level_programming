724621cfb4b2574518251d965a04dee077f4df57
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-19 21:05:33 +0000

    Change if condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		count++;
 	}
-	if (idx - count == 1)
+	if (idx == count)
 		return (add_dnodeint_end(h, n));
 	free(new);
 	return (NULL);
