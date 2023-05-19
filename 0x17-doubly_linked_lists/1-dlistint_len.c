c91cb14fe9d6665a47d716940883c9d30da2de7d
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-05-19 20:00:45 +0000

    Add documentation to code

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * dlistint_len - returns the length of a doubly linked list.
+ * @h: head of the list.
+ * Return: the lenght of the list.
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
