d160bd5b4460176a77e8a9919ee4585481cca92f
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-06-10 05:00:06 +0000

    Modify code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *current_item;
 
-	if (!ht || !new_item)
+	if (!new_item)
 		return (0);
 	current_item = ht->array[index];
 	if (current_item == NULL)
