446adb6a5daf6cd70a7f89912271fd0420a01640
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-06-10 16:56:46 +0000

    Add code to handle task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - gets an item.
+ * @ht: the hash table.
+ * @key: the key of the item to be retrieved.
+ * Return: the item or NULL if the function fails.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *item = ht->array[index];
+
+	if (item)
+	{
+		if (strcmp(item->key, key) == 0)
+			return (item->value);
+	}
+	return (NULL);
+}
