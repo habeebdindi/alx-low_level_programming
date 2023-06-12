427a4968b347f8a975e48b2b82874c8f76ee5d78
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-06-12 02:55:39 +0000

    Modify loop condition

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -10,7 +10,7 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i;
 	hash_node_t *temp, *temp_fr;
 
-	for (i = 0; i < ht->size; i++)
+	for (i = 0; i < ht->size & ht; i++)
 	{
 		temp = ht->array[i];
 		while (temp)
